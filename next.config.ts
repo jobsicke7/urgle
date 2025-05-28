b0522834144695ba6c7408b6881098b6a1c14d60
jobsicke7 <doh292929@gmail.com>
2025-05-28 15:17:26 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,22 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/look-alike/:path*',
+        destination: 'http://kgh1113.ddns.net/api/look-alike/:path*',
+      },
+      {
+        source: '/api/mood/:path*',
+        destination: 'http://kgh1113.ddns.net/api/mood/:path*',
+      },
+      {
+        source: '/socket.io/:path*',
+        destination: 'http://kgh1113.ddns.net/socket.io/:path*',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
