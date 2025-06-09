442c1cb8ef23d5342b97e745d0c4050353d98c90
jobsicke <doh292929@gmail.com>
2025-06-10 08:51:26 +0900

    fin.

--- src/components/HistoryList/HistoryList.tsx
+++ src/components/HistoryList/HistoryList.tsx
@@ -63,38 +63,37 @@ const HistoryList: React.FC<HistoryListProps> = ({ items }) => {
                 <div className={styles.latestImageRow}>
                   {item.imgUrl && (
                     <div className={styles.latestImageContainer}>
-                      {isErrorResult(item.alike) ? (
-                        <Image
-                          src="/img/noface.svg"
-                          alt="얼굴 인식 실패"
-                          layout="fill"
-                          objectFit="contain"
-                          className={`${styles.historyImage} ${styles.latest}`}
-                        />
-                      ) : (
-                        <Image
-                          src={item.resultImgUrl}
-                          alt={item.alike}
-                          layout="fill"
-                          objectFit="cover"
-                          className={`${styles.historyImage} ${styles.latest}`}
-                          unoptimized={item.resultImgUrl.startsWith('http') ? false : true}
-                        />
-                      )}
+                      <Image
+                        src={item.imgUrl}
+                        alt="촬영 사진"
+                        layout="fill"
+                        objectFit="cover"
+                        className={`${styles.historyImage} ${styles.latest}`}
+                      />
                     </div>
-
                   )}
                   <span className={`${styles.arrow} ${styles.latest}`}>&rarr;</span>
                   <div className={styles.latestImageContainer}>
-                    <Image
-                      src={item.resultImgUrl}
-                      alt={item.alike}
-                      layout="fill"
-                      objectFit="cover"
-                      className={`${styles.historyImage} ${styles.latest}`}
-                      unoptimized={item.resultImgUrl.startsWith('http') ? false : true}
-                    />
+                    {isErrorResult(item.alike) ? (
+                      <Image
+                        src="/img/noface.svg"
+                        alt="얼굴 인식 실패"
+                        layout="fill"
+                        objectFit="contain"
+                        className={`${styles.historyImage} ${styles.latest}`}
+                      />
+                    ) : (
+                      <Image
+                        src={item.resultImgUrl}
+                        alt={item.alike}
+                        layout="fill"
+                        objectFit="cover"
+                        className={`${styles.historyImage} ${styles.latest}`}
+                        unoptimized={item.resultImgUrl.startsWith('http') ? false : true}
+                      />
+                    )}
                   </div>
+
                 </div>
                 <div className={`${styles.info} ${styles.latestInfo}`}>
                   <span className={`${styles.celebrityName} ${styles.latest}`}>{item.alike}</span>
